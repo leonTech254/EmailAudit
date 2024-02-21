93ce714e4e647d4379e82069517a241ceae5765f
leonTech254 <martinleontech23@gmail.com>
2024-02-21 09:18:32 +0300

    --pass data to RecipientsDispayComponent

--- react/src/components/RecipientsDisplay.tsx
+++ react/src/components/RecipientsDisplay.tsx
@@ -1,1 +1,14 @@
-export default function RecipientsDisplay() {}
+// RecipientsDisplay.js
+import React from 'react';
+
+function RecipientsDisplay({ recipients }) {
+  return (
+    <span>
+      {recipients.map((recipient, index) => (
+        <span key={index}>{recipient}</span>
+      ))}
+    </span>
+  );
+}
+
+export default RecipientsDisplay;
